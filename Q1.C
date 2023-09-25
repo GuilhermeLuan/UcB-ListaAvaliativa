37f2219963a77d08947c6964d1327432bb6a551f
Guilherme Luan <guilhermeluan@pm.me>
2023-09-25 18:01:06 -0300

    fix: q1.c

--- Q1.C
+++ Q1.C
@@ -4,15 +4,13 @@ int main(){
     int ano;
     scanf("%d", &ano);
 
-    if(ano == 1940 || ano == 1944 || ano == 1942 || ano == 1946){
-        printf("Edicao cancelada devido a Segunda Guerra Mundial.\n");
-    } else if(ano == 1916){
-        printf("Edicao cancelada devido a Primeira Guerra Mundial.\n");
+    if(ano == 1940 || ano == 1944 || ano == 1942 || ano == 1946 || ano == 1916){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
     } else if ((ano >= 1896) && ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0) || ano == 1900)) {
         printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
     } else if ((ano >= 1930) && ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0) || (ano % 2 == 0 && ano % 100 != 0))){
             printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
     } else {
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d", ano);
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.", ano);
     }
 }
